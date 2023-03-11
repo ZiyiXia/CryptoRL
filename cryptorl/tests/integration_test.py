b1354d7a4b1b1bf27cca8a90cab4f8f2c8b2f46b
ZiyiXia <joeyxia0805@163.com>
2023-03-11 10:24:51 -0700

    format code

--- cryptorl/tests/integration_test.py
+++ cryptorl/tests/integration_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from cryptorl import tickers, fetch_single, fetch_multiple, get_arrays
 
+
 def test_fetching_process():
     tics = tickers("USD")[20:22]
     ret = fetch_multiple('2022-06-01-00-00', '2022-09-01-00-00', tics)
