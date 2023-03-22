88957a4d8cf68bf25d6cbd7ca5e65a011b71a867
ZiyiXia <joeyxia0805@163.com>
2023-03-22 19:37:15 -0400

    add split function

--- cryptorl/env_crypto.py
+++ cryptorl/env_crypto.py
@@ -2,8 +2,12 @@ import gym
 
 
 class CryptoEnv(gym.Env):
-    def __init__(self) -> None:
+    def __init__(self, init_asset) -> None:
         super().__init__()
+        self.capital = init_asset
+        self.balance = init_asset
+        self.terminal = False
+        self.episode = 0
 
     def reset(self):
         pass
