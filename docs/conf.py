91ffe8df64e9db574af8e369cbf209ffca97e50f
ZiyiXia <joeyxia0805@163.com>
2023-04-04 19:21:21 -0400

    keep changing

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sphinx_rtd_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(1, os.path.abspath('../cryptorl'))
+sys.path.insert(0, os.path.abspath('../cryptorl'))
 
 
 # -- Project information -----------------------------------------------------
