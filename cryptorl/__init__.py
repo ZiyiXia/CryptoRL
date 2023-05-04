dc5cca95002dfd673f6c354523b4352cda59ec39
ZiyiXia <joeyxia0805@163.com>
2023-05-03 20:29:51 -0400

    fix dependencies

--- cryptorl/__init__.py
+++ cryptorl/__init__.py
@@ -1,7 +1,7 @@
-from data import tickers, fetch_single, fetch_multiple, get_arrays
-from utils import get_feature_combinations, cal_avg_rank
-from predict_LR import test_all_lr
-from predict_DT import dt_pruning, dt_feature_importance
-from predict_NN import test_all_nn
+from .data import tickers, fetch_single, fetch_multiple, get_arrays
+from .utils import get_feature_combinations, cal_avg_rank
+from .predict_LR import test_all_lr
+from .predict_DT import dt_pruning, dt_feature_importance
+from .predict_NN import test_all_nn
 
 __version__ = "0.1.0"
